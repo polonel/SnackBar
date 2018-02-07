3797f9f77f93463a3097cdc37726dcf617f5b6f1
Alex Viscreanu <alexviscreanu@gmail.com>
2018-02-07 01:51:36 +0100

    Added demo html In order to make it easier for someone who forks the repo and wants to try/fix things

---
+++ demo/src/init.js
@@ -0,0 +1,65 @@
+(function ($) {
+  $(function () {
+    $('ul.tabs').tabs();
+    $('.snackbar').click(function(e) {
+      e.preventDefault();
+      var pos = $(this).attr('data-pos');
+      var actionText = $(this).attr('data-actionText');
+      var actionColor = $(this).attr('data-actionColor');
+      if (actionText == null)
+        actionText = 'Dismiss'
+      if (actionColor == null)
+        actionColor = '#f66496';
+      var showAction = $(this).attr('data-showAction');
+      if (showAction == null)
+        showAction = true;
+      else if (showAction == 'true')
+        showAction = true;
+      else
+        showAction = false;
+
+      Snackbar.show({
+        text: 'Welcome! Thanks for checking out Snackbar',
+        showAction: showAction,
+        actionText: actionText,
+        actionTextColor: actionColor,
+        backgroundColor: '#232323',
+        width: 'auto',
+        pos: pos
+      });
+    });
+
+    $('.snackbar-callback').click(function() {
+      Snackbar.show({
+        text: 'I have a custom callback when action button is clicked.',
+        width: '475px',
+        onActionClick: function(element) {
+          //Set opacity of element to 0 to close Snackbar
+          $(element).css('opacity', 0);
+          Snackbar.show({
+            text: 'Thanks for clicking the  <strong>Dismiss</strong>  button!',
+            showActionButton: false
+          });
+        }
+      });
+    });
+
+    $('.button-collapse').sideNav();
+    $('.scrollspy').scrollSpy();
+
+    $('html').niceScroll({
+      cursorcolor: '#323232',
+      cursorwidth: '5px',
+      zindex: 9999,
+      cursorborder: '#323232'
+    });
+
+    $('tbody').niceScroll({
+      cursorcolor: '#e1d4de',
+      cursorwidth: '5px',
+      zindex: 9999,
+      cursorborder: '#e1d4de'
+    });
+  }); // end of document ready
+
+})(jQuery); // end of jQuery name space
